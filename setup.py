ec8e77f3ca8cbfb2f43e381f9d85c668346ac9cc
Gemma Hentsch <gemma@halfapenguin.com>
2017-07-25 12:42:35 +0100

    initial skeleton

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.rst") as readme_file:
+    long_description = readme_file.read()
+
+
+setup(name="Loaded Random",
+    version="0.1",
+    packages=["loaded",],
+    license="GNU GPL v3.0",
+    description="Given a list of probabilities and outputs at instantiation, generates random outputs based on the probabilities",
+    author="Gemma Hentsch",
+    author_email="contact@halfapenguin.com",
+    long_description=long_description,
+    keywords = ['statistics','random'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+    ]
+)
